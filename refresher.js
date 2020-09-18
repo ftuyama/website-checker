bb26b56403953b7ac9344b5aee80f3bf5f608d14
Felipe Tuyama <felipe.tuyama@thetrainline.com>
2020-09-18 13:56:25 +0100

    Initial commit

---
+++ refresher.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+    var audioUrl = chrome.runtime.getURL(`assets/cash1.wav`);
+    var audio = new Audio(audioUrl);
+    var audioCtx = new AudioContext();
+    var source = audioCtx.createMediaElementSource(audio);
+
+    // create a gain node
+    var gainNode = audioCtx.createGain();
+    gainNode.gain.value = 20;
+    source.connect(gainNode);
+
+    // connect the gain node to an output destination
+    gainNode.connect(audioCtx.destination);
+
+    console.log(new Date().toISOString());
+    setTimeout(() => {
+        var LookFor = "Não há horários disponíveis no momento. Tente novamente mais tarde!";
+        if($('body:contains("' + LookFor + '")').length == 0) {
+            audio.play();
+            alert("Found: " + LookFor);
+        }
+        else {
+            location.reload();
+        }
+    }, 30000);
+});
